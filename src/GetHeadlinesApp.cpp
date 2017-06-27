fb9173eb1497c61ccb859f95536a81ec33ea623d
Kat Sullivan <kmsullivan012@gmail.com>
2017-06-27 16:39:36 -0400

    parse out html links

--- src/GetHeadlinesApp.cpp
+++ src/GetHeadlinesApp.cpp
@@ -8,11 +8,10 @@
 #include "cinder/Utilities.h"
 #include "jsoncpp/json.h"
 #include "twitcurl.h"
+#include "cinder/params/Params.h"
 
 #include <fstream>
-
-// won't always need this - just for debugging
-#include <typeinfo>
+#include <regex>
 
 using namespace ci;
 using namespace ci::app;
@@ -24,17 +23,23 @@ class GetHeadlinesApp : public App {
     void prepareSettings(Settings* settings);
     
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+    
+    void getTweets(string &resp);
     
     //We'll parse our twitter content into these
     vector<string> temp;
     vector<string> words;
+    vector<string> names;
     
     //For drawing our text
     gl::TextureFont::DrawOptions fontOpts;
-    gl::TextureFontRef font;
+    gl::TextureFontRef mFont;
+    
+    // TODO ADD params
+    // TODO add text box to search for particular term from networks we are already searching
+    params::InterfaceGl	mParams;
     
     twitCurl twit;
     vector<std::string> tweets;
@@ -42,10 +47,26 @@ class GetHeadlinesApp : public App {
     string keys[4];     // hold keys for Twitter API oauth
 };
 
+void GetHeadlinesApp::prepareSettings(Settings* settings)
+{
+    settings->setWindowSize(1280, 720);
+    cout << "def working1" << endl;
+}
+
 void GetHeadlinesApp::setup()
 {
+    setFullScreen(true);
     gl::clear(Color(0, 0, 0));
     gl::enableAlphaBlending(false);
+    
+    // TODO - change fonts
+    mFont = gl::TextureFont::create(Font( "Times New Roman", 32 ));
+    
+    // TODO -  tv networks & donald trump
+    names = {"realDonaldTrump", "FoxNews"};
+    
+    // TODO - list of keywords to futher curate tweets
+    // TODO - get lots more tweets
     
     int count = 0;
     string line;
@@ -70,25 +91,38 @@ void GetHeadlinesApp::setup()
     
     if(twit.accountVerifyCredGet())
     {
-        twit.getLastWebResponse(resp);
-        vector<string> names = {"realDonaldTrump", "FoxNews"};
+        // TODO - make go through ea networks timeline instead?
         if(twit.userLookup(names, false)){
             twit.getLastWebResponse(resp);
-            std::cout << typeid(resp).name() << '\n';
-            std::ofstream file("filename.json");
-            file << resp;
-            file.close();
-            
-            Json::Reader reader;
             Json::Value root;
-            std::ifstream inputFile("filename.json");
-            inputFile >> root;
-            for(auto s : root){
-                std::string t = "@" + s["screen_name"].asString() + ":" + s["status"]["text"].asString();
-                tweets.push_back(t);
+            Json::Reader json;
+            bool parsed = json.parse(resp, root, false);
+            
+            if(!parsed) {
+                console() << json.getFormattedErrorMessages() << endl;
+            } else {
+                const Json::Value results = root;
+                for(auto s: root)
+                {
+                    // logo and then tweet
+                    std::string t = s["status"]["text"].asString();
+                    // TODO - this isn't perfect test with .@SenSchumer: "Senate Republican healthcare bill is a wolf in sheep's clothing, only this wolf has even sharper teeth than the House bill."
+                    size_t end = t.find("http");
+                    string editedTweet = t.substr(0, end);
+                    cout << editedTweet << endl;
+                    tweets.push_back(editedTweet);
+                }
             }
-            inputFile.close();
         }
+        // search with query
+//        if(twit.search("Donald")){
+//            twit.getLastWebResponse(resp);
+//            cout << resp << endl;
+//        } else {
+//            cout << "DID NOT" << endl;
+//            twit.getLastCurlError(resp);
+//            console() << resp << endl;
+//        }
     }
     else
     {
@@ -98,14 +132,9 @@ void GetHeadlinesApp::setup()
     
 }
 
-void GetHeadlinesApp::prepareSettings(Settings* settings)
+void GetHeadlinesApp::getTweets(string &resp)
 {
-    settings->setWindowSize(1280, 720);
-    cout << "def working1" << endl;
-}
-
-void GetHeadlinesApp::mouseDown( MouseEvent event )
-{
+    
 }
 
 void GetHeadlinesApp::update()
@@ -114,12 +143,23 @@ void GetHeadlinesApp::update()
 
 void GetHeadlinesApp::draw()
 {
+    // TODO - get flag image for reference
     gl::clear( Color( 0, 0, 0 ) );
     int counter = 25;
+    
+    // TODO - black text on white & white text on red
+    // TODO - scrolling, ea line is one network
+    // TODO - fit into height & width via andrew
+    // TODO - send to Syphon
+    // TODO - Syphon to isadora
+    // TODO - trump put him on bottom
     for(std::string s : tweets) {
         gl::drawString(s, vec2(10, counter));
         counter+=25;
     }
 }
+
+// TODO - clickable app that can work on any comp
+// TODO - app will need params that can be edited easily
 
 CINDER_APP( GetHeadlinesApp, RendererGl )
