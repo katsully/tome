5317c6901d2cc7aeff70f77571e9667e7549279f
Kat Sullivan <kmsullivan012@gmail.com>
2017-06-27 21:47:26 -0400

    added scrolling and tweets

--- src/GetHeadlinesApp.cpp
+++ src/GetHeadlinesApp.cpp
@@ -19,9 +19,6 @@ using namespace std;
 
 class GetHeadlinesApp : public App {
   public:
-    //Optional for setting app size
-    void prepareSettings(Settings* settings);
-    
 	void setup() override;
 	void update() override;
     void keyDown( KeyEvent event ) override;
@@ -29,42 +26,64 @@ class GetHeadlinesApp : public App {
     
     void getTweets(string &resp);
     
+    // background image
+    gl::TextureRef mBackground;
+    // star image
+    gl::TextureRef mStars;
+    // logos
+    vector<gl::TextureRef> mLogos;
+    
     //We'll parse our twitter content into these
     string tempKeyword;
     vector<string> mKeywords;
     vector<string> mAccounts;
     
     //For drawing our text
-    gl::TextureFont::DrawOptions fontOpts;
-    gl::TextureFontRef mFont;
+    Font mFont;
+    gl::TextureFontRef	mTextureFont;
     
-    // TODO add text box to search for particular term from networks we are already searching
     params::InterfaceGlRef mParams;
-    bool mShowParams = true;
+    bool mShowParams = false;
     
     twitCurl twit;
-    vector<std::string> tweets;
+    vector<vector<string>> mTweets;
     
-    string keys[4];     // hold keys for Twitter API oauth
+    // hold keys for Twitter API oauth
+    string keys[4];
+    
+    int stripeHeight;
+    int widthPos = 0;
+    int widthPosOffset = 0;
 };
-
-void GetHeadlinesApp::prepareSettings(Settings* settings)
-{
-    settings->setWindowSize(1280, 720);
-    cout << "def working1" << endl;
-}
 
 void GetHeadlinesApp::setup()
 {
     setFullScreen(true);
+    
+    stripeHeight = getWindowHeight()/13;
+
     gl::clear(Color(0, 0, 0));
     gl::enableAlphaBlending(false);
     
-    // TODO -  tv networks & donald trump
-    mAccounts = {"realDonaldTrump", "FoxNews"};
+    // load our flag image
+    mBackground = gl::Texture::create( loadImage( loadAsset("flag.jpg") ) );
+    // load just the stars
+    mStars = gl::Texture::create( loadImage( loadAsset("flag2.jpg")));
     
+    // load logos
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("fox.jpg"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("cnn.jpg"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("msnbc.jpg"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("abc.png"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("nbc.png"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("cbs.jpg"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("blaze.png"))));
+    mLogos.push_back(gl::Texture::create( loadImage( loadAsset("dt.jpg"))));
+    
+    // TODO -  tv networks & donald trump
     // TODO - these should prob live in a sep file
-    mKeywords = {"Donald", "Trump", "Flint", "Sessions", "Healthcare"};
+    mAccounts = {"FoxNews", "CNNPolitics", "MSNBC", "ABC", "NBCNews", "CBSNews", "theblaze", "realDonaldTrump"};
+    mKeywords = {"Donald", "Trump", "Flint", "Sessions", "Healthcare", "POTUS", "Syrians", "Syria", "Senate", "GOP", "Republicans", "Democrats", "President", "health care", "president", "ObamaCare", "Obama", "N. Korea", "Senators", "Congress", "Putin", "Hillary", "Pelosi", "McConnell", "China"};
     
     // Create the interface and give it a name
     mParams = params::InterfaceGl::create("App parameters", vec2(200,200));
@@ -73,11 +92,8 @@ void GetHeadlinesApp::setup()
     mParams->addParam( "New Keyword", &tempKeyword ).updateFn( [this] { mKeywords.push_back(tempKeyword);} );
     mParams->addParam("Show Params", &mShowParams).key("p");
     
-    // TODO - change fonts
-    mFont = gl::TextureFont::create(Font( "Times New Roman", 32 ));
-    
-    // TODO - list of keywords to futher curate tweets
-    // TODO - get lots more tweets
+    mFont = Font( "Avenir", 36 );
+    mTextureFont = gl::TextureFont::create( mFont );
     
     int count = 0;
     string line;
@@ -102,84 +118,91 @@ void GetHeadlinesApp::setup()
     
     if(twit.accountVerifyCredGet())
     {
-        // TODO - make go through ea networks timeline instead?
-        if(twit.userLookup(mAccounts, false)){
-            twit.getLastWebResponse(resp);
-            Json::Value root;
-            Json::Reader json;
-            bool parsed = json.parse(resp, root, false);
-            
-            if(!parsed) {
-                console() << json.getFormattedErrorMessages() << endl;
-            } else {
-                const Json::Value results = root;
-                for(auto s: root)
-                {
-                    // logo and then tweet
-                    std::string t = s["status"]["text"].asString();
-                    // TODO - this isn't perfect test with .@SenSchumer: "Senate Republican healthcare bill is a wolf in sheep's clothing, only this wolf has even sharper teeth than the House bill."
-                    size_t end = t.find("http");
-                    string editedTweet = t.substr(0, end);
-                    cout << editedTweet << endl;
-                    tweets.push_back(editedTweet);
+        for(string a: mAccounts) {
+            if(twit.timelineUserGet(true, false, 20, a)) {
+                cout << a << endl;
+                vector<string> temp;
+                twit.getLastWebResponse(resp);
+                Json::Value root;
+                Json::Reader json;
+                bool parsed = json.parse(resp, root, false);
+                
+                if(!parsed) {
+                    console() << json.getFormattedErrorMessages() << endl;
+                } else {
+                    const Json::Value results = root;
+                    for(auto s: root)
+                    {
+                        std::string t = s["text"].asString();
+                        // TODO - this isn't perfect test with .@SenSchumer: "Senate Republican healthcare bill is a wolf in sheep's clothing, only this wolf has even sharper teeth than the House bill."
+                        for(string k: mKeywords){
+                            if (t.find(k) != std::string::npos) {
+                                size_t end2 = t.find("http");
+                                string editedTweet = t.substr(0, end2);
+                                cout << editedTweet << endl;
+                                temp.push_back(editedTweet);
+                                break;
+                            }
+                        }
+                    }
                 }
+                mTweets.push_back(temp);
             }
         }
-        // search with query
-//        if(twit.search("Donald")){
-//            twit.getLastWebResponse(resp);
-//            cout << resp << endl;
-//        } else {
-//            cout << "DID NOT" << endl;
-//            twit.getLastCurlError(resp);
-//            console() << resp << endl;
-//        }
     }
     else
     {
         twit.getLastCurlError(resp);
         console() << resp << endl;
     }
-    
 }
 
+// TODO - make this respond to an update button in params
 void GetHeadlinesApp::getTweets(string &resp)
 {
-    
 }
 
 void GetHeadlinesApp::update()
 {
 }
 
-void GetHeadlinesApp::keyDown( KeyEvent event ) {
-    for(string s: mKeywords) {
-        cout << s << endl;
-    }
+void GetHeadlinesApp::keyDown( KeyEvent event )
+{
 }
 
 void GetHeadlinesApp::draw()
 {
-    // TODO - get flag image for reference
-    gl::clear( Color( 0, 0, 0 ) );
-    int counter = 25;
+    gl::color(Color::white());
+    gl::draw( mBackground, getWindowBounds() );
+    int counter = 0;
     
-    // TODO - black text on white & white text on red
-    // TODO - scrolling, ea line is one network
     // TODO - fit into height & width via andrew
     // TODO - send to Syphon
     // TODO - Syphon to isadora
-    // TODO - trump put him on bottom
-    for(std::string s : tweets) {
-        gl::drawString(s, vec2(10, counter));
-        counter+=25;
+    // TODO - logo and then tweet
+    // TODO - figure out how to calculate width of tweet
+    for(vector<string> s : mTweets) {
+        (counter >= 7) ? widthPos = 10 : widthPos = getWindowWidth() / 2 - 20;
+        for(string s1: s) {
+            gl::color(Color::white());
+            Rectf logoRect(widthPos-widthPosOffset, counter*stripeHeight+5, widthPos-widthPosOffset+50, counter*stripeHeight+stripeHeight);
+            gl::draw(mLogos[counter], logoRect);
+            (counter%2==0) ? gl::color( Color::white() ) : gl::color( Color::black() );
+            mTextureFont->drawString(s1, vec2(widthPos-widthPosOffset+50, counter*stripeHeight+45));
+            widthPos+=s1.length()*18;
+        }
+        counter++;
     }
+    
+    widthPosOffset++;
+    gl::color(Color::white());
+    Rectf drawRect( 0, 0, getWindowWidth()*.48, getWindowHeight()*.55 );
+    gl::draw(mStars, drawRect);
     
     // Draw the interface
     if(mShowParams) { mParams->draw(); }
 }
 
 // TODO - clickable app that can work on any comp
-// TODO - app will need params that can be edited easily
 
 CINDER_APP( GetHeadlinesApp, RendererGl )
